d9753375efcea46b2c08783f8303c19c4b25c59e
Mostafa Eldaly <eldalymostafa068@gmail.com>
2025-09-19 16:08:33 +0300

    stream

---
+++ stream.dart
@@ -0,0 +1,13 @@
+
+Stream<int> numberStream() async* {
+  for (int i = 1; i <= 5; i++) {
+    await Future.delayed(Duration(seconds: 1));
+    yield i;
+  }
+}
+
+void main() async {
+  await for (var number in numberStream()) {
+    print(number);
+  }
+}
